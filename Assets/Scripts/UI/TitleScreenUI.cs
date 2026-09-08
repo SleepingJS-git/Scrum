f8ffa10fe6b571f8ebbbba78fc348d7b433c53f9
Stewart Jones <saj8580@rit.edu>
2026-09-08 00:20:15 -0400

    simple main menus FSM stuff (thinking bout dem lobby codes)

--- Assets/Scripts/UI/TitleScreenUI.cs
+++ Assets/Scripts/UI/TitleScreenUI.cs
@@ -1,11 +1,59 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+public enum MenuScreen
+{
+    Main,
+    HostLobby,
+    JoinLobby,
+    LobbyRoom
+}
+
 public class TitleScreen : MonoBehaviour
 {
+    [SerializeField] private GameObject mainScreen;
+    [SerializeField] private GameObject hostLobbyScreen;
+    [SerializeField] private GameObject joinLobbyScreen;
+    [SerializeField] private GameObject lobbyRoomScreen;
+
+    public void ShowScreen(MenuScreen screen)
+    {
+        mainScreen.SetActive(screen == MenuScreen.Main);
+        hostLobbyScreen.SetActive(screen == MenuScreen.HostLobby);
+        joinLobbyScreen.SetActive(screen == MenuScreen.JoinLobby);
+        lobbyRoomScreen.SetActive(screen == MenuScreen.LobbyRoom);
+    }
+
+    public void ShowMain()
+    {
+        ShowScreen(MenuScreen.Main);
+    }
+
+    public void ShowHostLobby()
+    {
+        ShowScreen(MenuScreen.HostLobby);
+    }
+
+    public void ShowJoinLobby()
+    {
+        ShowScreen(MenuScreen.JoinLobby);
+    }
+
+    public void ShowLobbyRoom()
+    {
+        ShowScreen(MenuScreen.LobbyRoom);
+    }
+
+    //Basic test for changing scenes with start game button
     [SerializeField] string sceneName = "LevelGrayboxing";
     public void StartGame()
     {
         SceneManager.LoadScene(sceneName);
     }
+
+    private void Start()
+    {
+        ShowScreen(MenuScreen.Main);
+    }
+
 }
