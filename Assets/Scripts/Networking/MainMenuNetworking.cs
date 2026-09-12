c4af3d21e8ae00e8c3bbd207aecbdfb6e89b37ff
Stewart Jones <saj8580@rit.edu>
2026-09-12 03:34:03 -0400

    added basic relay lobby creation with very little player feedback

--- Assets/Scripts/Networking/MainMenuNetworking.cs
+++ Assets/Scripts/Networking/MainMenuNetworking.cs
@@ -1,16 +1,50 @@
+using TMPro;
 using UnityEngine;
+using Unity.Services.Core;
+using Unity.Services.Authentication;
+using Unity.Services.Multiplayer;
 
-public class MultiplayerMenuController : MonoBehaviour
+public class MainMenuNetworking : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private TMP_Text hostCodeText;
+    [SerializeField] private TMP_InputField joinCodeInput;
+
+    //the session that this player is either hosting or joined to
+    private ISession currentSession;
+
+    //initializes unity cloud services
+    private async void Start()
     {
-        
+        await UnityServices.InitializeAsync();
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+
+        Debug.Log("networking online");
     }
 
-    // Update is called once per frame
-    void Update()
+    //creates a lobby w/ code upon clicking host lobby button
+    public async void HostLobby()
     {
-        
+        var options = new SessionOptions
+        {
+            MaxPlayers = 4
+        }.WithRelayNetwork();
+
+        currentSession =
+            await MultiplayerService.Instance.CreateSessionAsync(options);
+
+        hostCodeText.text = currentSession.Code;
+
+        Debug.Log($"created lobby: {currentSession.Code}");
+    }
+
+    //joins a lobby if code is correct
+    public async void JoinLobby()
+    {
+        string code = joinCodeInput.text.Trim().ToUpper();
+
+        currentSession =
+            await MultiplayerService.Instance.JoinSessionByCodeAsync(code);
+
+        Debug.Log("joined lobby");
     }
 }
