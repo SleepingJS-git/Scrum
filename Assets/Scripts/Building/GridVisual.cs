b98b1baba06d64665a8127718556144842e3cedf
SteventheBeven <smp6196@rit.edu>
2026-09-17 10:47:39 -0400

    grid visual in progress

--- Assets/Scripts/Building/GridVisual.cs
+++ Assets/Scripts/Building/GridVisual.cs
@@ -4,7 +4,13 @@ using UnityEngine.UIElements;
 public class GridVisual : MonoBehaviour
 {
     [SerializeField]
+    bool on;
+
+    [SerializeField]
     Grid grid;
+
+    [SerializeField]
+    Color gridColor;
 
     public int length = 10;
 
@@ -14,20 +20,21 @@ public class GridVisual : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.gray;
+        //if (!on) { return; }
+        Gizmos.color = gridColor;
 
-        for (int y = 0; y <= height; y++)
+        for (int y = -(height/2); y <= height/2; y++)
         {
-            for (int z = 0; z <= length; z++)
+            for (int z = -(length/2); z <= length/2 ; z++)
             {
-                Vector3 start = transform.position + new Vector3(0, y, z * grid.cellSize.z);
-                Vector3 end = transform.position + new Vector3(width * grid.cellSize.x, y, z * grid.cellSize.z);
+                Vector3 start = transform.position + new Vector3((-width/2), y, z * grid.cellSize.z);
+                Vector3 end = transform.position + new Vector3((width * grid.cellSize.x)/2, y, z * grid.cellSize.z);
                 Gizmos.DrawLine(start, end);
             }
-            for (int x = 0; x <= width; x++)
+            for (int x = -(length/2); x <= width/2; x++)
             {
-                Vector3 start = transform.position + new Vector3(x * grid.cellSize.x, y, 0);
-                Vector3 end = transform.position + new Vector3(x * grid.cellSize.x, y, length * grid.cellSize.z);
+                Vector3 start = transform.position + new Vector3(x * grid.cellSize.x, y, -(length/2));
+                Vector3 end = transform.position + new Vector3(x * grid.cellSize.x, y, (length * grid.cellSize.z)/2);
                 Gizmos.DrawLine(start, end);
             }
         }
