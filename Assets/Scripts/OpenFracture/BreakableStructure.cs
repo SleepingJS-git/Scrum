bad6129dde68d527beffd5bee0681aca96aaaa04
RyanCCG <24845973+LemonFeeb@users.noreply.github.com>
2026-09-19 14:37:31 -0400

    Tested if fracture can run at runtime

---
+++ Assets/Scripts/OpenFracture/BreakableStructure.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+public class BreakableStructure
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
